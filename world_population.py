bca87d5ed62ad6850042ffa7b26fcd07106892ed
Connor Foster <foster.clee@gmail.com>
2019-05-02 12:23:48 -0500

    create a dictionary of country data in for loop, add this to a pygal map, and then render to a svg file

--- world_population.py
+++ world_population.py
@@ -1,19 +1,27 @@
 import json
+import pygal.maps.world
 from country_codes import get_country_code
 
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
     
-# Print the 2010 population for each country.
+# Build a dictionary of country data
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print(code + ": "+ str(population))
+            cc_populations[code] = population
         else:
             print("ERROR - " + country_name)
+        
+        
+wm = pygal.maps.world.World()
+wm.title = 'World Population in 2010 by Country'
+wm.add('Countries', cc_populations)
+wm.render_to_file('world_populations_2010.svg')
             
         
